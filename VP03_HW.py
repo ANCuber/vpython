04f5d54192f2962f7d236d8d388573b45c489403
ancuber1031 <ansona1131@gmail.com>
2024-09-24 20:06:36 +0800

    modified hw3 9/24

--- VP03_HW.py
+++ VP03_HW.py
@@ -11,58 +11,85 @@ earth_orbit = {'r': 1.495E11, 'v': 2.9783E4}
 moon_orbit = {'r': 3.84E8, 'v': 1.022E3}
 theta = 5.145*pi/180.0
 sim_rate = 1000
-Slowing = 6
 
 #Set the scene
-scene = canvas(width=800, height=800, background=vector(0.5,0.5,0))
-scene.light = []
-local_light(pos = vec(0,0,0))
+scene = canvas(width=800, height=800, background=vector(0.5,0.5,0), align = 'left')
+#scene.light = []
+#local_light(pos = vec(0,0,0))
+
+#Celestial body class
+class c_body(sphere):
+    m = 1
+    v = vec(0,0,0)
+    a = vec(0,0,0)
+
+#Center of mass
+def CM(m1, m2, p1, p2):
+    return (m1*p1 + m2*p2)/(m1+m2)
 
 #Set celestial bodies
-Cme = (mass['moon']*vec(moon_orbit['r'],0,0))/(mass['earth']+mass['moon'])
-sun = sphere(pos = vec(0,0,0), m = mass['sun'], radius = size['sun'], color = color.orange, emissive = True)
-sun.v = vec(0,0,0)
-earth = sphere(pos = (-mass['moon']/(mass['earth']+mass['moon']))*vec(moon_orbit['r']*cos(theta),-moon_orbit['r']*sin(theta),0), m = mass['earth'], radius = size['earth'], make_trail = False, texture = {'file':textures.earth})
-earth.v = vec(0,0,moon_orbit['v'])*(mass['moon']/(mass['earth']+mass['moon']))
-moon = sphere(pos = (mass['earth']/(mass['earth']+mass['moon']))*vec(moon_orbit['r']*cos(theta),-moon_orbit['r']*sin(theta),0), m = mass['moon'], radius = size['moon'], make_trail = False)
-moon.v = vec(0,0,-moon_orbit['v'])*(mass['earth']/(mass['earth']+mass['moon']))
+sun = c_body(pos = vec(0,0,0), m = mass['sun'], radius = size['sun'], color = color.orange, emissive = True)
+earth = c_body(pos = vec(0,0,0), m = mass['earth'], radius = size['earth'], texture = {'file':textures.earth}, make_trail = True)
+moon = c_body(pos = vec(moon_orbit['r']*cos(theta),moon_orbit['r']*sin(theta),0), m = mass['moon'], radius = size['moon'])
 
-earth.pos += vec(earth_orbit['r']*(mass['sun']/(mass['sun']+mass['earth']+mass['moon'])),0,0)
-scene.center = earth.pos
-earth.make_trail = True
-moon.pos += vec(earth_orbit['r']*(mass['sun']/(mass['sun']+mass['earth']+mass['moon'])),0,0)
-#moon.make_trail = True
-sun.pos += vec(-earth_orbit['r']*((mass['earth']+mass['moon'])/(mass['sun']+mass['earth']+mass['moon'])),0,0)
+#position
+C_em = CM(earth.m,moon.m,earth.pos,moon.pos)
+earth.pos -= C_em
+moon.pos -= C_em
+earth.pos.x += earth_orbit['r']
+moon.pos.x += earth_orbit['r']
 
-earth.v += vec(0,0,-earth_orbit['v']*(mass['sun']/(mass['sun']+mass['earth']+mass['moon'])))
-moon.v += vec(0,0,-earth_orbit['v']*(mass['sun']/(mass['sun']+mass['earth']+mass['moon'])))
-sun.v = vec(0,0,earth_orbit['v']*((mass['earth']+mass['moon'])/(mass['sun']+mass['earth']+mass['moon'])))
+C_ems = CM(sun.m,earth.m+moon.m,sun.pos,vec(earth_orbit['r'],0,0))
+sun.pos -= C_ems
+earth.pos -= C_ems
+moon.pos -= C_ems
+
+#velocity
+earth.v += (moon.m)/(moon.m+earth.m)*vec(0,0,moon_orbit['v'])
+moon.v += (earth.m)/(moon.m+earth.m)*vec(0,0,-moon_orbit['v'])
+sun.v += (moon.m+earth.m)/(moon.m+earth.m+sun.m)*vec(0,0,earth_orbit['v'])
+earth.v += (sun.m)/(moon.m+earth.m+sun.m)*vec(0,0,-earth_orbit['v'])
+moon.v += (sun.m)/(moon.m+earth.m+sun.m)*vec(0,0,-earth_orbit['v'])
 
 #Point of view
 scene.forward = vec(-1,-1,-1)
+scene.center = earth.pos
 
-#Functions
+#G_Force funciton
 def G_Force(M, m, pos_vec):
     if mag(pos_vec) == 0:
         return vec(0,0,0)
     return -G*M*m/mag2(pos_vec)*norm(pos_vec)
 
 #Settings for simulation
-#bodies_c = [moon, earth, sun]
 c_bodies = [moon, earth, sun]
-bodies = [moon, earth]
-dt = 60*60*6
-dt /= Slowing
+dt = 60*60*6/100
+
+#Graph
+precession = graph(width = 500, align = 'right')
+angle = gdots(graph = precession, color = color.blue, radius = 1)
+#func2 = gcurve(graph = precession, color = color.green)
+
+ear = arrow(pos = earth.pos, color = color.red, shaftwidth = size['earth']*0.1)
+mar = arrow(pos = earth.pos, color = color.blue, shaftwidth = size['earth']*0.1)
 
 #Simulation
 print("Simulation begins.")
 
-mx, mn = -1, 1
 cnt = 0
+pre = 0
+esp = 1e-3
+ref = 2
 
 while True:
-    rate(sim_rate)
-    pre = moon.pos-earth.pos
+    rate(sim_rate*1000)
+
+    if cnt <= 1e6 and ref > dot(norm(cross(moon.pos-earth.pos,moon.v-earth.v)),vec(1,0,0)):
+        ref = dot(norm(cross(moon.pos-earth.pos,moon.v-earth.v)),vec(1,0,0))
+        pre = cnt
+    if cnt > 1e6 and abs(dot(norm(cross(moon.pos-earth.pos,moon.v-earth.v)),vec(1,0,0)) - ref) <= esp:
+        print("The precession period is about", (cnt-pre)*dt/86400/364.2422,"years.")
+        cnt = 0
     
     #Consider gravity
     for cbody in c_bodies:
@@ -73,20 +100,17 @@ while True:
         
         cbody.v += cbody.a*dt
         cbody.pos += cbody.v*dt
+    
+    if cnt%1000 == 0:
+        angle.plot(pos = (cnt*1e-6, dot(norm(cross(moon.pos-earth.pos,moon.v-earth.v)),vec(1,0,0))))
+    
 
-    aft = moon.pos-earth.pos
-    crs = cross(pre,aft)
-    cur = math.acos(dot(crs,vec(0,1,0))/mag(crs)*mag(vec(0,1,0)))
-    mx = max(mx,mag(earth.pos-sun.pos))
-    mn = min(mn,mag(earth.pos-sun.pos))
-
-    '''
-    if cnt >= sim_rate*120:
-        break
-    '''
+    #ear.pos = earth.pos
+    #mar.pos = earth.pos
+    #ear.axis = 2*size['earth']*vec(0,1,0)
+    #mar.axis = 2*size['earth']*norm(cross(moon.pos-earth.pos,moon.v-earth.v))
 
     scene.center = earth.pos
     cnt += 1
 
-print(mx/(pi/180), mn/(pi/180))
 
